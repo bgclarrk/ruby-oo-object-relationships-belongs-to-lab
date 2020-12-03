2931ba4165b25600ed350332164c944ba3058ec9
+ <+>
2020-12-03 16:07:16 -0700

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+    attr_accessor :title, :author
+
+    def author=(author)
+        @author = author
+    end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+    attr_accessor :name, :artist, :title
+
+    def artist=(artist)
+        @artist = artist
+    end
+
+end
